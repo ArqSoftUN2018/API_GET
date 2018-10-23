dd46e444dd6faa3ece05b47f1e4d065cd9901ec6
bdiazab <bdiazabuyc@gmail.com>
2018-10-23 18:06:33 -0500

    typeDefs changes

--- src/lists/typeDefs.js
+++ src/lists/typeDefs.js
@@ -3,29 +3,25 @@ export const listsTypeDef = `
 scalar String
 
 type List {
-    code: Int!
-    name: String!
-    archived: Boolean!
-    description: String!
-    created_at: String!
-    updated_at: String!
-    board_code: Int!
-
+    id: Int!
+    nombre: String!
+    archivado: String!
+    tablero: String!
 }
 input ListInput {
-    name: String!
-    archived: Boolean!
-    description: String!
-    board_code: Int!
+    id: Int
+    nombre: String!
+    archivado: String!
+    tablero: String!
 }`;
 
 export const listsQueries = `
     allLists: [List]!
-    listByCode(code: Int!): List!
+    listByCode(id: Int!): List!
 `;
 
 export const listsMutations = `
     createList(list: ListInput!): List!
-    deleteList(code: Int!): Int
-    updateList(code: Int!, list: ListInput!): List!
+    deleteList(id: Int!): Int
+    updateList(id: Int!, list: ListInput!): List!
 `;
