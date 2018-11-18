151c7c57b25c7c6a15167ed5307f45ff30d3f2ba
Bryan Antonio Angarita Rodriguez <baangaritar@unal.edu.co>
2018-11-18 01:04:22 -0500

    Add URL

--- src/boards/server.js
+++ src/boards/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.BOARDS_URL
+export const url = process.env.BOARDS_URL  || 'boards-ms';
 export const port = process.env.BOARDS_PORT
 export const entryPoint = process.env.BOARDS_ENTRY
