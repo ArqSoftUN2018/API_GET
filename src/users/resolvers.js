6d1b0600306c7d59bab21b24c6f47bbb9ee5effd
diego gomez <diagomezpa@unal.edu.co>
2018-12-06 10:34:31 -0500

    arreglando api

--- src/users/resolvers.js
+++ src/users/resolvers.js
@@ -1,4 +1,4 @@
-import { generalRequest, getRequest} from '../utilities';
+import { generalRequest, getRequest, generalRequest1 } from '../utilities';
 import { url, port, entryPoint } from './server';
 
 const URL = `http://${url}:${port}/${entryPoint}`;
@@ -10,7 +10,9 @@ const resolvers = {
 		userById: (_, { id }) =>
 			generalRequest(`${URL}/${id}`, 'GET'),
 		userToken: (_, { user }) =>
-			generalRequest(`http://${url}:${port}/get-token`, 'POST', user)
+			generalRequest(`http://${url}:${port}/get-token`, 'POST', user),
+		userInfo: (_,{ token }) =>
+		    generalRequest1(`http://${url}:${port}/user`, 'GET',_,token.token)
 	},
 	Mutation: {
 		createUser: (_, { user }) =>
