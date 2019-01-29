dfc076b42e97dddd3f775b1289c6826910db43de
bdiazab <bdiazabuyc@gmail.com>
2019-01-29 04:11:02 -0500

    users-ms

--- src/users/server.js
+++ src/users/server.js
@@ -1,3 +1,3 @@
 export const url = process.env.AUTHENTICATION_URL || 'users-ms';
 export const port = process.env.AUTHENTICATION_PORT || '4000';
-export const entryPoint = process.env.AUTHENTICATION_ENTRY || 'auth';
+export const entryPoint = process.env.AUTHENTICATION_ENTRY || 'users';
